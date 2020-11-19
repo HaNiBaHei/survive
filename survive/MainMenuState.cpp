8f0cffb5c6a3572e96a4269e2a007faec6486302
CHINNAPAT SIRIYAJAI <63010231@KMITL.AC.TH>
2020-11-19 19:36:45 +0700

    102 done

--- survive/MainMenuState.cpp
+++ survive/MainMenuState.cpp
@@ -50,22 +50,30 @@ void MainMenuState::initKeybinds()
 
 void MainMenuState::initButton()
 {
-	this->buttons["GAME_STATE"] = new gui::Button(960.f, 440.f, 250.f, 65.f,
+	this->buttons["GAME_STATE"] = new gui::Button(
+		this->p2pX(44.5f), this->p2pY(40.74f), 
+		this->p2pX(13.f), this->p2pX(3.5f),
 		&this->font, "New Game", 35, // font size
 		sf::Color(0, 0, 0, 250), sf::Color(250, 0, 0, 250), sf::Color(20, 20, 20, 50), // text color
 		sf::Color(70, 70, 70, 200), sf::Color(150, 150, 150, 200), sf::Color(20, 20, 20, 200)); // background color
 
-	this->buttons["SETTINGS_STATE"] = new gui::Button(960.f, 540.f, 250.f, 65.f,
+	this->buttons["SETTINGS_STATE"] = new gui::Button(
+		this->p2pX(44.5f), this->p2pY(50.f),
+		this->p2pX(13.f), this->p2pX(3.5f),
 		&this->font, "Settings", 35,
 		sf::Color(0, 0, 0, 250), sf::Color(250, 0, 0, 250), sf::Color(20, 20, 20, 50),
 		sf::Color(70, 70, 70, 200), sf::Color(150, 150, 150, 200), sf::Color(20, 20, 20, 200));
 
-	this->buttons["EDITOR_STATE"] = new gui::Button(960.f, 640.f, 250.f, 65.f,
+	this->buttons["EDITOR_STATE"] = new gui::Button(
+		this->p2pX(44.5f), this->p2pY(59.25f),
+		this->p2pX(13.f), this->p2pX(3.5f),
 		&this->font, "Editor", 35,
 		sf::Color(0, 0, 0, 250), sf::Color(250, 0, 0, 250), sf::Color(20, 20, 20, 50),
 		sf::Color(70, 70, 70, 200), sf::Color(150, 150, 150, 200), sf::Color(20, 20, 20, 200));
 
-	this->buttons["EXIT_STATE"] = new gui::Button(960.f, 840.f, 250.f, 65.f,
+	this->buttons["EXIT_STATE"] = new gui::Button(
+		this->p2pX(44.5f), this->p2pY(77.77f),
+		this->p2pX(13.f), this->p2pX(3.5f),
 		&this->font, "Quit", 35,
 		sf::Color(0, 0, 0, 250), sf::Color(250, 0, 0, 250), sf::Color(20, 20, 20, 50),
 		sf::Color(70, 70, 70, 200), sf::Color(150, 150, 150, 200), sf::Color(20, 20, 20, 200));
