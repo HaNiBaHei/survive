e224e574f5c45510fc1ebab25c0b2ae89aa3a20d
CHINNAPAT SIRIYAJAI <63010231@KMITL.AC.TH>
2020-11-21 20:07:53 +0700

    121 done

--- survive/Entity.cpp
+++ survive/Entity.cpp
@@ -7,6 +7,7 @@ void Entity::initVariables()
 	this->movementComponent = NULL;
 	this->animationComponent = NULL;
 	this->attributeComponent = NULL;
+	this->skillcomponent = NULL;
 }
 
 Entity::Entity()
@@ -20,6 +21,7 @@ Entity::~Entity()
 	delete this->movementComponent;
 	delete this->animationComponent;
 	delete this->attributeComponent;
+	delete this->skillcomponent;
 }
 
 // Components //
@@ -48,6 +50,11 @@ void Entity::createAnimationComponent(sf::Texture& texture_sheet)
 void Entity::createAttributeComponent(const unsigned level)
 {
 	this->attributeComponent = new AttributeComponent(level);
+}
+
+void Entity::createSkillComponent()
+{
+	this->skillcomponent = new SkillComponent();
 }
 
 const sf::Vector2f& Entity::getPosition() const
---
+++ survive/SkillComponent.h
@@ -0,0 +1,94 @@
+#ifndef SKILLCOMPONENT_H
+#define SKILLCOMPONENT_H
+
+class SkillComponent
+{
+private:	
+	class Skill
+	{
+	private:
+		int level;
+		int levelCap;
+		int exp;
+		int expNext;
+
+	public:
+		Skill()
+		{
+			this->level = 1;
+			this->levelCap = 99;
+			this->exp = 0;
+			this->expNext = 100;
+		}
+		~Skill() {}
+
+		// Accessors //
+		inline const int& getLevel() const { return this->level; }
+		inline const int& getExp() const { return this->exp; }
+		inline const int& getExpNext() const { return this->expNext; }
+
+		// Modifiers //
+		inline void setLevel(const int level) { this->level = level; }
+		inline void setLevelCap(const int level_cap) { this->level = level_cap; }
+
+
+		// Functions //
+		void gainExp(const int exp) 
+		{ 
+			this->exp += exp; 
+			this->updateLevel();
+		}
+
+		void loseExp(const int exp)
+		{
+			this->exp -= exp;
+		}
+		
+		void updateLevel(const bool up = true)
+		{
+			/**
+			* Increases or decreases level of the skill
+			* 
+			*/
+			if (up)
+			{
+				if (this->level < this->levelCap)
+				{
+					while (this->exp >= this->expNext)
+					{
+						if (this->level < this->levelCap)
+						{
+							this->level++;
+							this->expNext = std::pow(this->level, 2) * 10 + this->level * 2 + this->level;
+						}
+					}
+				}
+			}
+			else
+			{
+				if (this->level > 0)
+				{
+					while (this->exp < 0)
+					{
+						if (this->level > 0)
+						{
+							this->level--;
+							this->expNext = std::pow(this->level, 2) * 10 + this->level * 2 + this->level;
+						}
+					}
+				}
+			}
+		}
+		void update()
+		{
+			
+		}
+
+	};
+
+public:
+	SkillComponent();
+	virtual ~SkillComponent();
+};
+
+#endif 
